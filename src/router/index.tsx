829140531c7fd52ca3ee4be1a9c9667a66361bf4
ash <chxmeleonweng@gmail.com>
2022-12-14 12:15:38 +0800

    [BUILD] change path ./ to @/

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,30 +1,28 @@
 import { Routes, Route, HashRouter } from 'react-router-dom'
-import Login from '../pages/Login'
-import List from '../pages/List'
-import Rooms from '../pages/Rooms/index'
-import Room from '../pages/Rooms/[id]'
-import Streamers from '../pages/Streamers'
-import Streamer from '../pages/Streamers/[id]'
-import Girls from '../pages/Girls'
-import Profile from '../pages/Profile'
+import Login from '@/pages/Login'
+// import List from '../pages/List'
+// import Rooms from '../pages/Rooms/index'
+// import Room from '../pages/Rooms/[id]'
+// import Streamers from '../pages/Streamers'
+// import Streamer from '../pages/Streamers/[id]'
+// import Girls from '../pages/Girls'
+// import Profile from '../pages/Profile'
 
 const Router: React.FC = () => {
   return (
-    <>
-      <HashRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Rooms />} />
-          <Route path="/rooms/:id" element={<Room />} />
-          <Route path="/streamers" element={<Streamers />} />
-          <Route path="/streamers/:id" element={<Streamer />} />
-          <Route path="/girls" element={<Girls />} />
-          <Route path="/lists" element={<List />} />
-          <Route path="/rankings" element={<List />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </HashRouter>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/" element={<Rooms />} /> */}
+        {/* <Route path="/rooms/:id" element={<Room />} /> */}
+        {/* <Route path="/streamers" element={<Streamers />} /> */}
+        {/* <Route path="/streamers/:id" element={<Streamer />} /> */}
+        {/* <Route path="/girls" element={<Girls />} /> */}
+        {/* <Route path="/lists" element={<List />} /> */}
+        {/* <Route path="/rankings" element={<List />} /> */}
+        {/* <Route path="/profile" element={<Profile />} /> */}
+      </Routes>
+    </HashRouter>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,7 +17,9 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
  
   
