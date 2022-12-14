fd16ff45a8fdee2a40cf4fab3765a0e185b35699
ash <chxmeleonweng@gmail.com>
2022-12-14 10:52:17 +0800

    [BUILD] create vite project

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}',
+      './src/styles/*.{scss, css}'
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+ 
+  
