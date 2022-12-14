0509dea2d399b29f56011b6d166607848287ab15
ash <chxmeleonweng@gmail.com>
2022-12-15 01:32:30 +0800

    [STYLE] adjust sidebar button layout

--- src/layouts/LeftSidebar.tsx
+++ src/layouts/LeftSidebar.tsx
@@ -1,25 +1,25 @@
 import LogoImg from '@/assets/login_logo.webp'
-import { LinkButton } from '@/components/Button'
+import { LinkButton, ToggleButton } from '@/components/Button'
 
 const LeftSidebar = () => {
   return (
-    <div className="flex flex-grow py-1 px-4 w-52 text-center border-r-[0.5px] border-r-theme-75">
-      <div className="mx-auto w-full h-full flex flex-col justify-around">
-        <section>
+    <div className="flex flex-grow py-5 px-4 w-52 text-center border-r-[0.5px] border-r-theme-75">
+      <div className="mx-auto w-full h-full">
+        <section className="pb-5 w-full">
           <div className="flex">
             <img src={LogoImg} alt="logo image" className="m-auto w-24" />
           </div>
-          <div className="flex justify-around items-center w-full">
+          <div className="flex justify-around items-center pt-6 w-full">
             <div className="mt-1.5 w-11 h-11 i-heroicons-user-circle-solid"></div>
             <div className="text-left">
-              <div>username</div>
-              <div>points</div>
+              <p className="text-xl">username</p>
+              <p className="text-theme-300">points</p>
             </div>
           </div>
         </section>
-        <hr className="border-1 border-theme-75" />
-        <section className="w-full h-1/2 ">
-          <div className="w-full h-1/2 flex flex-col justify-around">
+        <hr className="py-1 border-t-1 border-t-theme-75" />
+        <section className="w-full h-80">
+          <div className="flex flex-col justify-evenly w-full h-full">
             <LinkButton href="/">
               <p>百家樂</p>
             </LinkButton>
@@ -37,12 +37,34 @@ const LeftSidebar = () => {
             </LinkButton>
           </div>
         </section>
-        <hr className="border-1 border-theme-75" />
-        <section>
-          <div>會員公告</div>
-          <div>遊戲說明</div>
-          <div>客服中心</div>
-          <div>APP下載</div>
+        <hr className="my-4 border-1 border-theme-75" />
+        <section className="w-full h-40">
+          <div className="flex flex-col justify-around w-full h-full">
+            <ToggleButton>
+              <div className="flex justify-around items-center m-auto w-2/3">
+                <div className="text-lg i-heroicons-megaphone-solid"></div>
+                <p>會員公告</p>
+              </div>
+            </ToggleButton>
+            <ToggleButton>
+              <div className="flex justify-around items-center m-auto w-2/3">
+                <div className="text-lg i-heroicons-information-circle"></div>
+                <p>遊戲說明</p>
+              </div>
+            </ToggleButton>
+            <ToggleButton>
+              <div className="flex justify-around items-center m-auto w-2/3">
+                <div className="text-lg i-heroicons-phone-arrow-up-right-solid"></div>
+                <p>客服中心</p>
+              </div>
+            </ToggleButton>
+            <ToggleButton>
+              <div className="flex justify-around items-center m-auto w-2/3">
+                <div className="text-lg i-mdi-cellphone-arrow-down-variant"></div>
+                <p>APP下載</p>
+              </div>
+            </ToggleButton>
+          </div>
         </section>
       </div>
     </div>
