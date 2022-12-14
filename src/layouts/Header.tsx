ce27ea13876d0214066e7b948aa1bbb7c8b78403
ash <chxmeleonweng@gmail.com>
2022-12-14 17:47:51 +0800

    [FEATURE] install unocss icon and add condition to image bg

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+  return (
+    <header className="relative">
+      <nav className="absolute left-0 top-0 bg-[#131313] h-12 w-full">
+        <div className="i-mdi-alarm"></div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
