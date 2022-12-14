d655a80d4ef2f0c7cedde33855a632f4e44a7f3f
ash <chxmeleonweng@gmail.com>
2022-12-14 19:16:10 +0800

    [FEATURE] add icon to header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,10 +1,18 @@
 
-
 const Header = () => {
   return (
     <header className="relative">
       <nav className="absolute left-0 top-0 bg-[#131313] h-12 w-full">
-        <div className="i-mdi-alarm"></div>
+        <div className="flex justify-end items-center px-3 w-full h-full">
+          <div className="flex justify-around text-2xl w-[14rem]">
+            <button className="i-mdi-heart hover:text-pink-500"></button>
+            <button className="i-mdi-account hover:text-theme-300"></button>
+            <button className="i-mdi-file-document hover:text-theme-300"></button>
+            <button className="i-mdi-chart-pie hover:text-theme-300"></button>
+            <button className="i-mdi-web hover:text-theme-300"></button>
+            <button className="i-mdi-refresh hover:text-theme-300"></button>
+          </div>
+        </div>
       </nav>
     </header>
   )
