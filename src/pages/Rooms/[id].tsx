3776fabfebd70ae0e45ad340f510616d20292d5f
ash <chxmeleonweng@gmail.com>
2022-12-22 02:36:06 +0800

    [FEATURE] add bet desk component

--- src/pages/Rooms/[id].tsx
+++ src/pages/Rooms/[id].tsx
@@ -1,17 +1,25 @@
 import RoomTestImage from '@/assets/test.webp'
+import BetDesk from '@/components/BetDesk'
 import ChatRoom from '@/components/Chatroom'
 import { clsx as cx } from 'clsx'
 
 const Room = () => {
-  const bgImage = cx`bg-cover bg-center w-full h-full bg-no-repeat bg-[url('../assets/room-bg.webp')]`
+  const bgImage = cx`absolute  bg-cover bg-center w-full h-full bg-no-repeat bg-[url('../assets/room-bg.webp')]`
 
   return (
     <div className="relative w-full h-full">
       <div className="relative w-full h-4/5">
         <div className={bgImage}></div>
-        <div className="w-12 h-12 border-2 border-white -skew-x-12"></div>
+        <div className="flex relative flex-col justify-between items-center w-full h-full z-[2]">
+          <div className="w-full h-[56%] flex justify-center">
+            <div className="bg-black/80 aspect-video"></div>
+          </div>
+          <div className="flex-grow py-6 px-2 w-full">
+            <BetDesk />
+          </div>
+          <div className="w-full h-10 bg-gray-900">tool</div>
+        </div>
       </div>
-
       <div className="flex w-full h-1/5 bg-gray-800">
         <div className="flex justify-start w-full">
           <div>
@@ -29,7 +37,7 @@ const Room = () => {
           <div></div>
         </div>
         <div></div>
-        <div className=" w-50">
+        <div className="w-50">
           <ChatRoom />
         </div>
       </div>
