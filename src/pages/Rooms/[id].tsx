ef221df04e118e3194d8d70fb7b3450d8d9bbef7
ash <chxmeleonweng@gmail.com>
2022-12-26 18:38:40 +0800

    [FEATURE] adjust way that import chips image file

--- src/pages/Rooms/[id].tsx
+++ src/pages/Rooms/[id].tsx
@@ -4,32 +4,14 @@ import ChatRoom from '@/components/Chatroom'
 import { FormattedMessage } from 'react-intl'
 import { clsx as cx } from 'clsx'
 import { useSetup } from '@/hooks/useSetup'
-import { MouseEventHandler, useRef, useState } from 'react'
+import { useState } from 'react'
 import { BetButton } from '@/components/Button'
-import chips_10 from '../../assets/chips/chips_10.webp'
-import chips_100 from '../../assets/chips/chips_100.webp'
-import chips_500 from '../../assets/chips/chips_500.webp'
-import chips_1K from '../../assets/chips/chips_1K.webp'
-import chips_5K from '../../assets/chips/chips_5K.webp'
-import chips_1W from '../../assets/chips/chips_1W.webp'
-import chips_5W from '../../assets/chips/chips_5W.webp'
-import chips_10W from '../../assets/chips/chips_10W.webp'
+import { chipsImg } from '@/components/BetDesk/chips'
 
 const Room = () => {
   const [isToggle, setIsToggle] = useState<boolean>(false)
   const onToggle = () => setIsToggle((isToggle) => !isToggle)
   const { handleRegularToggle, isRegular } = useSetup()
-
-  const chipsImg = [
-    chips_10,
-    chips_100,
-    chips_500,
-    chips_1K,
-    chips_5K,
-    chips_1W,
-    chips_5W,
-    chips_10W,
-  ]
 
   const [betPrice, setBetPrice] = useState('')
   const handleSelectBetPrice = (e: React.MouseEvent) => {
