e511c55fbb40aae6a8b80274738a2efd81aee20c
ash <chxmeleonweng@gmail.com>
2023-01-08 02:32:37 +0800

    [FEATURE] add emoji picker and add scroll to bottom feature

--- src/components/room/Chatroom/index.tsx
+++ src/components/room/Chatroom/index.tsx
@@ -32,8 +32,14 @@ const ChatRoom = () => {
   }, [clickRef, isPickerShow])
 
   const scrollToBottom = () => {
-    messageRef?.scrollIntoView({ behavior: 'smooth' })
+    if (messageRef) {
+      messageRef.scrollIntoView({ behavior: 'smooth' })
+    }
   }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [scrollToBottom])
 
   const onPicked = (emoji: EmojiClickData, event: MouseEvent) => {
     setNewMessage(newMessage + emoji.emoji)
@@ -42,9 +48,11 @@ const ChatRoom = () => {
   const handleSendMessage = (e: React.FormEvent) => {
     e.preventDefault()
     if (newMessage === '') {
-      e.preventDefault()
+      e.stopPropagation()
     } else {
-      setMessages((messages) => [...messages, newMessage])
+      setTimeout(() => {
+        setMessages((messages) => [...messages, newMessage])
+      }, 150)
     }
     setNewMessage('')
   }
@@ -53,14 +61,29 @@ const ChatRoom = () => {
     <div className="flex flex-col w-full h-full bg-gray-50 border-b-2 border-gray-500">
       <div className="flex overflow-x-hidden flex-col-reverse flex-grow-0 w-full h-screen">
         <div
+          ref={setMessageRef}
           id="chat-content"
-          className="flex flex-col justify-end items-stretch w-full h-full scroll-smooth"
+          className="flex flex-col justify-end w-full h-full"
         >
-          {/* <div ref={setMessageRef}></div> */}
           {messages.map((item, idx) => {
             return (
-              <div className="p-1 px-5 w-full" key={idx}>
-                <p className="text-theme-50">{item}</p>
+              <div
+                className="flex justify-start items-start py-1 px-2.5 w-full h-auto text-theme-50"
+                key={idx}
+              >
+                <div className="flex-shrink-0 pr-1.5 pt-[1.5px]">
+                  <div className="w-6 h-6 bg-gray-400 rounded-full"></div>
+                </div>
+                <p className="pr-2">username</p>
+                <div className="inline-flex justify-start items-start">
+                  <div className="w-2 rounded-t-lg translate-y-2 -rotate-[9deg] -translate-x-[1px]">
+                    <div className="w-0 h-0 border-t-[5px] border-t-transparent border-b-[20px] border-b-transparent border-r-[20px] border-r-neutral-200"></div>
+                  </div>
+                  <div className="break-all rounded-lg border-r-2 border-b-2 bg-neutral-200 drop-shadow-sm border-b-theme-50/10">
+                    <p className="py-2 px-4">{item}</p>
+                  </div>
+                </div>
+                <p className="self-end pl-3 text-gray-400">time</p>
               </div>
             )
           })}
@@ -68,33 +91,37 @@ const ChatRoom = () => {
       </div>
       <form
         onSubmit={handleSendMessage}
-        className="flex relative flex-grow justify-between items-center px-1.5 mt-1 mb-2 w-full"
+        className="flex relative flex-grow justify-between items-center px-2 mt-1 mb-2 w-full"
       >
-        <div className="relative px-2 w-full h-full text-theme-50">
+        <div className="inline-flex relative items-center px-0.5 mx-2 w-full h-full bg-gray-200 rounded-lg text-theme-50">
           <input
             id="chat-input"
             type="text"
             placeholder="Send message"
             value={newMessage}
             onChange={(e) => setNewMessage(e.target.value)}
-            className="py-1 px-3 w-full h-full bg-gray-200 rounded-lg outline-transparent"
+            className="py-2 pl-3 pr-2 w-full h-full outline-transparent"
             autoComplete="off"
           />
+
+          <div ref={setClickRef}>
+            <div
+              className={`${
+                isPickerShow ? '' : 'hidden'
+              } absolute bottom-10 right-0 z-30`}
+            >
+              <EmojiPicker autoFocusSearch={false} onEmojiClick={onPicked} />
+            </div>
+            <div className="px-1">
+              <div
+                onClick={onTrigglerPicker}
+                className="text-2xl i-mdi-emoticon-happy text-theme-50/40"
+              ></div>
+            </div>
+          </div>
         </div>
-        <div className="flex h-full">
+        <div className="flex px-1 h-full">
           <button className="m-auto text-2xl text-amber-400 i-heroicons-paper-airplane-20-solid"></button>
-        </div>
-        <div className="absolute bottom-0 right-10 z-20 justify-end items-center h-full pt-[1px]">
-          <button
-            onClick={onTrigglerPicker}
-            className="text-2xl i-mdi-emoticon-happy text-theme-50/40"
-          ></button>
-        </div>
-        <div
-          ref={setClickRef}
-          className={`${isPickerShow ? '' : 'hidden'} absolute bottom-8 z-30 `}
-        >
-          <EmojiPicker autoFocusSearch={false} onEmojiClick={onPicked} />
         </div>
       </form>
     </div>
