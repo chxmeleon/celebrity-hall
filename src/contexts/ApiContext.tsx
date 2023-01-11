7ae5f2b4602a06c40e2317345815225f6a238a8e
Wayne Huang <wayne123tw@gmail.com>
2023-01-12 02:48:41 +0800

    [FEATURE] add authToken params to apollo

--- src/contexts/ApiContext.tsx
+++ src/contexts/ApiContext.tsx
@@ -1,10 +1,13 @@
 import { ApolloProvider } from '@apollo/client'
 import React from 'react'
+import { useLocalStorage } from 'usehooks-ts'
 import { createApolloClient } from '../helpers/apollo'
 
 export const ApiProvider: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
-  const apolloClient = createApolloClient()
+  const [authToken] = useLocalStorage<string | null>('user', null)
+  const apolloClient = createApolloClient(authToken)
+
   return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>
 }
