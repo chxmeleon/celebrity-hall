c253b9c607cd652fc4c090f044cb99bfb2b2d02e
Wayne Huang <wayne123tw@gmail.com>
2023-01-12 03:43:17 +0800

    [FEATURE] seperate flipcard & card

--- src/components/room/PockerResult/FlipCard.tsx
+++ src/components/room/PockerResult/FlipCard.tsx
@@ -1,7 +1,44 @@
 import React, { useEffect, useState } from 'react'
 
 type CardProps = {
-  data: []
+  data: string[]
+}
+
+const Card: React.FC<{
+  item: string
+  rotate90?: boolean
+  isOpen: boolean
+  index: number
+}> = ({ item, rotate90, index, isOpen }) => {
+  const [isCardOpen, setIsCardOpen] = useState(false)
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => {
+        setIsCardOpen(true)
+      }, 1000 * index)
+    } else {
+      setIsCardOpen(false)
+    }
+  }, [index, isOpen])
+
+  return (
+    <div className={rotate90 ? 'rotate-90' : ''}>
+      <div
+        className={`${
+          isCardOpen ? 'rotate-y-0' : '-rotate-y-180'
+        } transition-all duration-300 ease-in relative w-10`}
+      >
+        <img src={`/cards/${item}.webp`} alt="pocker image" />
+        <div
+          className={`absolute top-0 left-0 transition-all duration-300 ease-in ${
+            isCardOpen ? 'z-[-1] rotate-y-180' : 'z-0 rotate-y-0'
+          }`}
+        >
+          <img src="/cards/card_b.webp" alt="card back" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 const FlipCard: React.FC<CardProps> = ({ data }) => {
@@ -20,45 +57,15 @@ const FlipCard: React.FC<CardProps> = ({ data }) => {
   return (
     <div className="flex flex-grow justify-center pt-2 w-full">
       <div className="flex flex-wrap justify-evenly w-5/6">
-        {data?.map((item, idx) => {
-          return (
-            <div className="" key={idx}>
-              {idx === 2 ? (
-                <div className="rotate-90">
-                  <div
-                    className={`${
-                      isOpen ? 'rotate-y-0' : '-rotate-y-180'
-                    } transition-all duration-300 ease-in relative w-10`}
-                  >
-                    <img src={`/cards/${item}.webp`} alt="pocker image" />
-                    <div
-                      className={`absolute top-0 left-0 transition-all duration-300 ease-in ${
-                        isOpen ? 'z-[-1] rotate-y-180' : 'z-0 rotate-y-0'
-                      }`}
-                    >
-                      <img src="/cards/card_b.webp" alt="card back" />
-                    </div>
-                  </div>
-                </div>
-              ) : (
-                <div
-                  className={`${
-                    isOpen ? 'rotate-y-0' : '-rotate-y-180'
-                  } transition-all duration-300 ease-in relative w-10`}
-                >
-                  <img src={`/cards/${item}.webp`} alt="pocker image" />
-                  <div
-                    className={`absolute top-0 left-0 transition-all duration-300 ease-in ${
-                      isOpen ? 'z-[-1] rotate-y-180' : 'z-0 rotate-y-0'
-                    }`}
-                  >
-                    <img src="/cards/card_b.webp" alt="card back" />
-                  </div>
-                </div>
-              )}
-            </div>
-          )
-        })}
+        {data?.map((item, idx) => (
+          <Card
+            key={idx}
+            isOpen={isOpen}
+            index={idx}
+            item={item}
+            rotate90={idx === 2}
+          />
+        ))}
       </div>
     </div>
   )
