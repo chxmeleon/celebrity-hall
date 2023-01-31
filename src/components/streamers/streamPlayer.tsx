56be14c36dbb5c74dc7a61a2a47f2df5bb00121f
ash <chxmeleonweng@gmail.com>
2023-01-31 17:29:22 +0800

    [FEATURE] remove unused nodeplayer component in streamer

--- src/components/streamers/streamPlayer.tsx
+++ src/components/streamers/streamPlayer.tsx
@@ -8,43 +8,7 @@ export interface RoomStreamProps {
   soundOn?: boolean
   videoOn?: boolean
   autoSize?: boolean
-  isWebRTC?: boolean
   isLoading?: boolean
-}
-
-export const NodePlayerStream: React.FC<RoomStreamProps> = ({
-  streamName,
-  streamKey,
-  resolution,
-  soundOn,
-  videoOn,
-  autoSize,
-  isLoading
-}) => {
-  useEffect(() => {
-    const player = new NodePlayer()
-    NodePlayer.debug(false)
-    player.setView(`video-${streamName}-${streamKey}`)
-    player.setBufferTime(0)
-    player.setScaleMode(0)
-    player.setVolume(1.0)
-    player.setKeepScreenOn()
-    player.start(`https://rtc-tw.vvip99.net/${streamName}/${streamKey}.flv`)
-    player.on('error', (err) => {
-      console.error('playerError', err)
-    })
-  }, [streamName, streamKey])
-
-  return (
-    <div className="flex absolute z-0 flex-col w-full h-full">
-      <div className="overflow-hidden relative w-full h-full">
-        <canvas
-          id={`video-${streamName}-${streamKey}`}
-          className="w-full h-auto aspect-film"
-        />
-      </div>
-    </div>
-  )
 }
 
 export const WebRTCStream: React.FC<RoomStreamProps> = ({
