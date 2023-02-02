964de8babe07cdf93d9ba6fa9d06dfb5b6ba766c
ash <chxmeleonweng@gmail.com>
2023-02-02 17:52:12 +0800

    [FEATURE] rename incorrect function's name

--- src/pages/Rooms/[id].tsx
+++ src/pages/Rooms/[id].tsx
@@ -39,7 +39,7 @@ import RoomStreamMobile from '@/components/room/RoomStream/StreamMobile'
 import ChatroomMobile from '@/components/room/Chatroom/ChatroomMobile'
 import SinglePlayerMobile from '@/components/room/BetDesk/SinglePlayerMobile'
 import RoomDataContext from '@/contexts/RoomDataContext'
-import Loading from '@/components/room/RoomStream/loading'
+import Loading from '@/components/room/RoomStream/Loading'
 import { Tooltip } from '@material-tailwind/react'
 
 export const ChipButtonList: React.FC<{
